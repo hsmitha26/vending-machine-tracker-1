744f537b59d0a65f5d7cf7396f81d8cf337fbc99
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-24 10:31:37 -0600

    add passing test and features to show all locations for a snack and avg price of all snacks for each location

--- spec/features/snack_show_spec.rb
+++ spec/features/snack_show_spec.rb
@@ -18,15 +18,40 @@ RSpec.describe "When I visit a snack show page" do
     @machine_3  = @owner.machines.create!(location: "Machine Three")
 
     @snack_1 = Snack.create!(name: "Kit Kat", price: 1.00)
-    
-    MachineSnack.create!(snack_id: @snack_1, machine_id: @machine_1)
-    MachineSnack.create!(snack_id: @snack_1, machine_id: @machine_2)
-    MachineSnack.create!(snack_id: @snack_1, machine_id: @machine_3)
+    MachineSnack.create!(snack: @snack_1, machine: @machine_1)
+    MachineSnack.create!(snack: @snack_1, machine: @machine_2)
+    MachineSnack.create!(snack: @snack_1, machine: @machine_3)
+
+    @snack_2 = Snack.create!(name: "Twix", price: 1.50)
+    MachineSnack.create!(snack: @snack_2, machine: @machine_1)
+    MachineSnack.create!(snack: @snack_2, machine: @machine_2)
+
+    @snack_3 = Snack.create!(name: "Snickers", price: 2.50)
+    MachineSnack.create!(snack: @snack_3, machine: @machine_2)
+    MachineSnack.create!(snack: @snack_3, machine: @machine_3)
   end
 
-  it "I see the name of that snack" do
+  it "I see the name and price of that snack" do
     visit snack_path(@snack_1)
     expect(current_path).to eq(snack_path(@snack_1))
     expect(page).to have_content(@snack_1.name)
+    expect(page).to have_content(@snack_1.price)
+  end
+
+  it "I see a list of locations with vending machines that carry that snack and average price for snacks in each machine" do
+    visit snack_path(@snack_1)
+
+    within ("#machines-#{@machine_1.id}") do
+      expect(page).to have_content("Machine One")
+      expect(page).to have_content("$#{@machine_1.average_price}")
+    end
+    within ("#machines-#{@machine_2.id}") do
+      expect(page).to have_content("Machine Two")
+      expect(page).to have_content("$#{@machine_2.average_price}")
+    end
+    within ("#machines-#{@machine_3.id}") do
+      expect(page).to have_content("Machine Three")
+      expect(page).to have_content("$#{@machine_3.average_price}")
+    end
   end
 end
